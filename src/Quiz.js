9ff19c0de25e9e28a0c6d37246e9793598e76288
Mohaymen Ahmed <mohaymen.ahmed@avanade.com>
2019-03-06 15:54:20 -0500

    Reset quiz is complete

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,10 @@ class Quiz extends Component {
         this.state = {quiz_position: 1}
     }
 
+    handleResetClick() {
+        this.setState({ quiz_position: 1 })
+    }
+
     showNextQuestion() {
         this.setState((state) => {
             return {quiz_position: state.quiz_position + 1}
@@ -20,7 +24,7 @@ class Quiz extends Component {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length) //determine if we completed all questions
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} 
+                {isQuizEnd ? <QuizEnd resetClickHandler = {this.handleResetClick.bind(this)}/> : <QuizQuestion quiz_question = {quizData.quiz_questions[this.state.quiz_position - 1]} 
                 showNextQuestionHandler={this.showNextQuestion.bind(this)}/>}
             </div>
         )
